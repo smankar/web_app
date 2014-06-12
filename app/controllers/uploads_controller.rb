63ac041080b95f73a8a96bd2a8d03c06b4bec2fa
Sandeep Mankar <smankar@thoughtworks.com>
2014-06-12 16:47:00 +0000

    fix bug

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,21 +1,27 @@
-# encoding: UTF-8
 class UploadsController < ApplicationController
 
   def import
-
     file = params[:file]
-    if ".xls" == File.extname(file.original_filename)
-      spreadsheet = self.open_spreadsheet(file)
+    if file && ".xls" == File.extname(file.original_filename)
+      spreadsheet = open_spreadsheet(file)
       flash[:success] = "File Uploaded Successfully"
     else
-      flash[:danger] = "File not supported"
+      flash[:danger] = "Invalid File. Please upload a valid file!"
     end
 
     redirect_to root_url
   end
 
-  def open_spreadsheet(file)
-    Roo::Excel.new(file.path, nil, :ignore)
-  end
+  #def generate
+  #  @competition = ResultsInCompetition.find(params[:competition_id])
+  #  respond_to do |format|
+  #    format.pdf { prawnto :filename => filename, :prawn => {:page_layout => :landscape}}
+  #  end
+  #end
+
+  private
+    def open_spreadsheet(file)
+      Roo::Excel.new(file.path, nil, :ignore)
+    end
 
 end
