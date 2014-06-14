cb0c44743aa51c7f29c2a76ca6e04660b6f813e6
Sandeep Mankar <smankar@thoughtworks.com>
2014-06-14 08:25:35 +0000

    add local text

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class UploadsController < ApplicationController
 
   def import
@@ -5,7 +6,7 @@ class UploadsController < ApplicationController
     if file && ".xls" == File.extname(file.original_filename)
       spreadsheet = open_spreadsheet(file)
 
-      @spreadsheet_text = spreadsheet.cell(1,1)
+      @spreadsheet_text = spreadsheet.cell(6,3)
       send_data pdf,
                 :filename => "file.pdf",
                 :type => "application/pdf"
@@ -22,10 +23,40 @@ class UploadsController < ApplicationController
     end
 
     def pdf
-      content = @spreadsheet_text
+      header1 = "राम महिला बचत गॅट"
+      header2 = "ता. पाटण जी. सतारा"
+      header3 = "विषय: घरपोच आहार (ट.ह.र) पुरवठा केलेबाबत."
+      header4 = "२०१४-१५"
+      left1 = "बाल विकास प्रकल्प अधिकारी "
+      left2 = "एकात्मिक बाल विकास सेवा योजना, जिल्हा सातारा "
+      left3 = "आंगणवाडी केंद्राचे नावं:"
+      left4 = @spreadsheet_text
+      left5 = "वरील विषयी विनंती कि, आपल्या आदेशप्रमाणे अंगणवाडी केंद्रास खालील तपशीप्रमाणे"
+      left6 = " (Take Home Ration) "
+      left7 = "पुरवठा करण्यास आला आहे "
       Prawn::Document.new do
-        font(Rails.root.join('app', 'assets', 'fonts', 'Dvsr3ntt.ttf'), :size => 12)
-        text content
+        font_families.update("mangal" => {
+            :normal => Rails.root.join('app', 'assets', 'fonts', 'mangal.ttf')
+        },"surekh" => {
+            :normal => Rails.root.join('app', 'assets', 'fonts', 'Dvsr3ntt.ttf')
+        }
+        )
+        font("mangal") do
+         text header1, :size => 24, :align => :center
+         text header2, :size => 14, :align => :center
+         text header3, :size => 14, :align => :center
+         text header4, :size => 12, :align => :center
+         move_down 10
+         text left1, :size => 8
+         text left2, :size => 8
+        end
+        formatted_text [ { :text => left3, :font => "mangal",  :size => 8 },
+                          { :text => left4, :font => "surekh", :size => 14 }
+                          ]
+        formatted_text [ { :text => left5, :font => "mangal",  :size => 8 },
+                         { :text => left6, :font => "Courier", :size => 8 },
+                         { :text => left7, :font => "mangal", :size => 8 }
+                       ]
       end.render
     end
 
